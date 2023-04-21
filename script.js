e8af567fb9c408d83c26f333d54de8dce9a48af0
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:02:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+//your JS code here. If required
+const table = document.querySelector('table tbody');
+
+const promise1 = new Promise((resolve) => {
+  const time = Math.random() * 2000 + 1000;
+  setTimeout(() => resolve(time), time);
+});
+
+const promise2 = new Promise((resolve) => {
+  const time = Math.random() * 2000 + 1000;
+  setTimeout(() => resolve(time), time);
+});
+
+const promise3 = new Promise((resolve) => {
+  const time = Math.random() * 2000 + 1000;
+  setTimeout(() => resolve(time), time);
+});
+
+Promise.all([promise1, promise2, promise3])
+  .then((times) => {
+    const total = times.reduce((acc, curr) => acc + curr, 0);
+    const totalSeconds = total / 1000;
+
+    table.innerHTML = `
+      <tr>
+        <td>Promise 1</td>
+        <td>${(times[0] / 1000).toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Promise 2</td>
+        <td>${(times[1] / 1000).toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Promise 3</td>
+        <td>${(times[2] / 1000).toFixed(3)}</td>
+      </tr>
+      <tr>
+        <td>Total</td>
+        <td>${totalSeconds.toFixed(3)}</td>
+      </tr>
+    `;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
